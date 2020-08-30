70e2758325aa93f0609f2178134ccb0fa6393dbc
dmbclub <dmb437026158@gmail.com>
2020-08-31 01:09:59 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        String[] Tasks = new String[100];
         Scanner input = new Scanner(System.in);
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        String userInput = input.next();
+        String userInput = input.nextLine();
         while(!userInput.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
-            userInput = input.next();
+            //Print the list if input is 'list'
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for(int i = 0; i < count; i++) {
+                    System.out.println(i + 1 + ". " + Tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+            //Add the task into the task array
+            else {
+                Tasks[count] = userInput;
+                System.out.println("____________________________________________________________");
+                System.out.println("added:" + userInput);
+                System.out.println("____________________________________________________________");
+                count++;
+            }
+            userInput = input.nextLine();
         }
 
         System.out.println("____________________________________________________________");
