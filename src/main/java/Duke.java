5c8ebb9a33ed0c275853b58e1f3276c6156e1b61
dmbclub <dmb437026158@gmail.com>
2020-08-31 00:49:31 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        String userInput = input.next();
+        while(!userInput.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+            userInput = input.next();
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
