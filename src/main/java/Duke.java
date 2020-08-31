dfccaa30f1b2f41fe182a0822ae621215690cf73
dmbclub <dmb437026158@gmail.com>
2020-08-31 21:07:15 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-        }
-
-        public void markAsDone() {
-            this.isDone = true;
-        }
-    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int count = 0; //total number of tasks in the list
+        int count = 0; //total number of tasks in the list - 1
         Task[] Tasks = new Task[100];
         Greet();
         String userInput = input.nextLine();
@@ -29,19 +12,43 @@ public class Duke {
             //Print the list if input is 'list'
             if (userInput.equals("list")) {
                 listTasks(count, Tasks);
+                userInput = input.nextLine();
+                continue;
             }
             //Mark the tasks as done
             else if (userInput.startsWith("done")) {
                 markAsDone(Tasks, userInput);
+                userInput = input.nextLine();
+                continue;
+            }
+            else if (userInput.startsWith("deadline")) {
+                userInput = userInput.replace("deadline ", "");
+                String[] words = userInput.split(" /by ");
+                Tasks[count] = new Deadline(words[0], words[1]);
+                count++;
+            }
+            else if (userInput.startsWith("event")) {
+                userInput = userInput.replace("event ", "");
+                String[] words = userInput.split(" /at ");
+                Tasks[count] = new Event(words[0], words[1]);
+                count++;
             }
             //Add the task into the task array
             else {
-                Tasks[count] = new Task(userInput);
-                System.out.println("____________________________________________________________");
-                System.out.println("added:" + userInput);
-                System.out.println("____________________________________________________________");
+                userInput = userInput.replace("todo ", "");
+                Tasks[count] = new Todo(userInput);
                 count++;
             }
+            System.out.println("____________________________________________________________");
+            System.out.println("Got it. I've added this task:");
+            System.out.println(Tasks[count-1]);
+            if (count == 1){
+                System.out.println("Now you have " + count + " task in the list");
+            }
+            else {
+                System.out.println("Now you have " + count + " tasks in the list");
+            }
+            System.out.println("____________________________________________________________");
             userInput = input.nextLine();
         }
 
@@ -57,7 +64,7 @@ public class Duke {
         for (int j = 1; j < words.length; j++) {
             int index = Integer.parseInt(words[j]) - 1;
             tasks[index].markAsDone();
-            System.out.println(".[" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+            System.out.println("[" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
         }
         System.out.println("____________________________________________________________");
     }
@@ -66,7 +73,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < count; i++) {
-            System.out.println(i + 1 + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
+            System.out.println((i + 1) + "." + tasks[i]);
         }
         System.out.println("____________________________________________________________");
     }
