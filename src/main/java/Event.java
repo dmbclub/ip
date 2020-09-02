656306ad588a329a143b5a5f5409ac721d64db1b
dmbclub <dmb437026158@gmail.com>
2020-09-02 13:29:33 +0800

    Level-4

---
+++ src/main/java/Event.java
@@ -0,0 +1,21 @@
+public class Event extends Task {
+
+    private String time;
+
+    public Event(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + time + ")";
+    }
+}
