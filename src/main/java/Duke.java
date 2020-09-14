08eef4e1280155c136aaf96ce080d9ba998225ce
dmbclub <dmb437026158@gmail.com>
2020-09-15 06:21:43 +0800

    Level-6 A-Collections

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    static int MAX_NUM_TASK = 100;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int count = 0; //total number of tasks in the list - 1
-        Task[] tasks = new Task[MAX_NUM_TASK];
+        ArrayList<Task> tasks = new ArrayList<>();
         greet();
         String userInput = input.nextLine();
 
         while(!userInput.equals("bye")) {
             //Print the list if input is 'list'
             if (userInput.equals("list")) {
-                listTasks(count, tasks);
+                listTasks(tasks.size(), tasks);
                 userInput = input.nextLine();
                 continue;
             }
@@ -32,6 +31,21 @@ public class Duke {
                 userInput = input.nextLine();
                 continue;
             }
+            else if (userInput.startsWith("delete")) {
+                try {
+                    deleteTasks(tasks, userInput, tasks.size() - 1);
+                } catch (NumberFormatException e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" ☹ OOPS!!! Please enter a number after 'delete' command.");
+                    System.out.println("____________________________________________________________");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" ☹ OOPS!!! Please enter valid numbers after 'delete' command.");
+                    System.out.println("____________________________________________________________");
+                }
+                userInput = input.nextLine();
+                continue;
+            }
             else if (userInput.startsWith("deadline")) {
                 try {
                     String check = userInput.split(" ")[1];
@@ -44,7 +58,7 @@ public class Duke {
                 }
                 try {
                     String[] words = userInput.replace("todo ", "").split(" /by ");
-                    tasks[count] = new Deadline(words[0], words[1]);
+                    tasks.add(tasks.size(), new Deadline(words[0], words[1]));
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("____________________________________________________________");
                     System.out.println(" ☹ OOPS!!! You also need to specify the time of a deadline.");
@@ -52,7 +66,7 @@ public class Duke {
                     userInput = input.nextLine();
                     continue;
                 }
-                count++;
+                userInput = getAddTaskMessage(input, tasks.size(), tasks);
             }
             else if (userInput.startsWith("event")) {
                 try {
@@ -66,7 +80,7 @@ public class Duke {
                 }
                 try {
                     String[] words = userInput.replace("todo ", "").split(" /at ");
-                    tasks[count] = new Event(words[0], words[1]);
+                    tasks.add(tasks.size(), new Event(words[0], words[1]));
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("____________________________________________________________");
                     System.out.println(" ☹ OOPS!!! You also need to specify the time of an event.");
@@ -74,7 +88,7 @@ public class Duke {
                     userInput = input.nextLine();
                     continue;
                 }
-                count++;
+                userInput = getAddTaskMessage(input, tasks.size(), tasks);
             }
             //Add the task into the task array
             else if (userInput.startsWith("todo")) {
@@ -87,32 +101,38 @@ public class Duke {
                     userInput = input.nextLine();
                     continue;
                 }
-                tasks[count] = new Todo(userInput.replace("todo ", ""));
-                count++;
+                tasks.add(tasks.size(), new Todo(userInput.replace("todo ", "")));
+                userInput = getAddTaskMessage(input, tasks.size(), tasks);
             }
-            System.out.println("____________________________________________________________");
-            try {
-                System.out.println(tasks[count - 1]);
-                if (count == 1) {
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("Now you have " + count + " task in the list");
-                } else {
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("Now you have " + count + " tasks in the list");
-                }
-            } catch (ArrayIndexOutOfBoundsException e) {
+            else {
+                System.out.println("____________________________________________________________");
                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println("____________________________________________________________");
+                userInput = input.nextLine();
             }
-            System.out.println("____________________________________________________________");
-            userInput = input.nextLine();
         }
-
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 
-    private static void markAsDone(Task[] tasks, String userInput) {
+    private static String getAddTaskMessage(Scanner input, int count, ArrayList<Task> tasks) {
+        String userInput;
+        System.out.println("____________________________________________________________");
+        System.out.println(tasks.get(count - 1));
+        if (count == 1) {
+            System.out.println("Got it. I've added this task:");
+            System.out.println("Now you have " + count + " task in the list");
+        } else {
+            System.out.println("Got it. I've added this task:");
+            System.out.println("Now you have " + count + " tasks in the list");
+        }
+        System.out.println("____________________________________________________________");
+        userInput = input.nextLine();
+        return userInput;
+    }
+
+    private static void markAsDone(ArrayList<Task> tasks, String userInput) {
         String[] words = userInput.split(" ");
         if (words.length == 1) {
             System.out.println("____________________________________________________________");
@@ -124,17 +144,17 @@ public class Duke {
         System.out.println("Nice! I've marked this task as done:");
         for (int j = 1; j < words.length; j++) {
             int index = Integer.parseInt(words[j]) - 1;
-            tasks[index].markAsDone();
-            System.out.println("[" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+            tasks.get(index).markAsDone();
+            System.out.println("[" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).description);
         }
         System.out.println("____________________________________________________________");
     }
 
-    private static void listTasks(int count, Task[] tasks) {
+    private static void listTasks(int count, ArrayList<Task> tasks) {
         System.out.println("____________________________________________________________");
         System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < count; i++) {
-            System.out.println((i + 1) + "." + tasks[i]);
+            System.out.println((i + 1) + "." + tasks.get(i));
         }
         System.out.println("____________________________________________________________");
     }
@@ -144,5 +164,30 @@ public class Duke {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    private static void deleteTasks(ArrayList<Task> tasks, String userInput, int count) {
+        String[] words = userInput.split(" ");
+        switch (words.length) {
+            case 1:
+                System.out.println("____________________________________________________________");
+                System.out.println(" ☹ OOPS!!! You are not deleting any task.");
+                System.out.println("____________________________________________________________");
+                break;
+            case 2:
+                System.out.println("____________________________________________________________");
+                System.out.println("Noted. I've removed this task:");
+                int index = Integer.parseInt(words[1]) - 1;
+                System.out.println(tasks.get(index));
+                tasks.remove(index);
+                System.out.println(("Now you have " + count + " tasks in the list").replace("1 tasks", "1 task"));
+                System.out.println("____________________________________________________________");
+                break;
+            default:
+                System.out.println("____________________________________________________________");
+                System.out.println(" ☹ OOPS!!! Please enter just one number after 'delete' command.");
+                System.out.println("____________________________________________________________");
+                break;
+        }
     }
 }
